6b763da8c9cda2eb12893d3ee4ffb43fa1c6c295
Xoan Sampaiño <xoansampainho@gmail.com>
2011-05-08 16:41:55 +0200

    Unused SQL files

--- database/blocks.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS sports;
-DROP TABLE IF EXISTS blocks;
-CREATE TABLE blocks (
-	id integer primary key auto_increment,
-	name varchar(100),
-	friendly_name varchar(100) unique,
-	lat_avg decimal(11, 8),
-	lon_avg decimal(11, 8)
-);
--- database/sports.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE sports (
-	id integer primary key auto_increment,
-	name varchar(100),
-	location varchar(255),
-	block_id integer,
-	lat decimal(11, 8),
-	lon decimal(11, 8),
-	FOREIGN KEY (block_id) REFERENCES blocks(id)
-);
