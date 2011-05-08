bdfd383006e703b936f69a7965741f5186602088
Xoan Sampaiño <xoansampainho@gmail.com>
2011-05-08 14:00:41 +0200

    fix data type in mysql create table statement

--- database/blocks.sql
+++ database/blocks.sql
@@ -1,8 +1,9 @@
+DROP TABLE IF EXISTS sports;
 DROP TABLE IF EXISTS blocks;
 CREATE TABLE blocks (
 	id integer primary key auto_increment,
 	name varchar(100),
 	friendly_name varchar(100) unique,
-	lon_avg numeric,
-	lat_avg numeric
+	lon_avg decimal(11, 8),
+	lat_avg decimal(11, 8)
 );
--- database/sports.sql
+++ database/sports.sql
@@ -1,10 +1,9 @@
-DROP TABLE IF EXISTS sports;
 CREATE TABLE sports (
 	id integer primary key auto_increment,
 	name varchar(100),
 	location varchar(255),
 	block_id integer,
-	lon numeric,
-	lat numeric,
+	lon decimal(11, 8),
+	lat decimal(11, 8),
 	FOREIGN KEY (block_id) REFERENCES blocks(id)
 );
